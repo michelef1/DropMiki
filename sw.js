2d4ae495811bd1ce108de5dd20e057b2301d6f4d
michelef1 <michelef654321@gmail.com>
2026-09-14 18:29:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME='dropmiki-v20';
+const CORE=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ const u=new URL(e.request.url);
+ if(u.origin!==location.origin)return;
+ e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))));
+});
