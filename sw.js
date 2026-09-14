0f9156603798234be77b198aa7b917e89597a8f3
michelef1 <michelef654321@gmail.com>
2026-09-14 18:48:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'dropmiki-v15';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-192.png',
+  './icons/icon-maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      // fetch con {cache:'reload'} ignora la cache HTTP del browser:
+      // senza questo, un file vecchio già in cache HTTP verrebbe
+      // "fotografato" di nuovo anche dentro la nuova cache del SW
+      await Promise.all(
+        ASSETS.map(async (url) => {
+          const response = await fetch(url, { cache: 'reload' });
+          await cache.put(url, response);
+        })
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  // Non cachare mai le chiamate al broker PeerJS o ad altri host: devono restare live
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+      );
+    })
+  );
+});
